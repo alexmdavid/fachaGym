0de07b36319c39c485d8f9cd51a4142162c77333
alexdavid <joelarga2020@gmail.com>
2025-05-05 22:07:42 -0500

    tratando de solucionar el error 500 al intentar crear usuario

--- Program.cs
+++ Program.cs
@@ -21,8 +21,6 @@ builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
 var app = builder.Build();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
 
 if (app.Environment.IsDevelopment())
 {
